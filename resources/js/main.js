5003235ae393e9897ca6d7fa99bba419d93a6709
CRYDEsigN <Cryden_da@mail.ru>
2017-09-06 10:17:54 +0300

    v 1.1.0

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,7 @@
 try {
-    window.$ = window.jQuery = require('jquery');
+    window.$ = window.jQuery = require( 'jquery' );
+    require( './../../node_modules/bootstrap/dist/js/bootstrap.min.js' );
+} catch ( e ) {}
 
-    require('./../../node_modules/bootstrap/dist/js/bootstrap.min.js');
-} catch (e) {}
+console.log( 'script inits' );
 
-
-//require('./../../node_modules/jquery/dist/jquery.min.js');
-console.log('script inits');
-
-//require('./../../node_modules/bootstrap/dist/js/bootstrap');
-//console.log('script init');
